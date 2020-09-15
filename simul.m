ff575f4e85b984a369e862320725f102ca23af61
FINNSTAR7 <39476026+FINNSTAR7@users.noreply.github.com>
2020-09-15 16:04:52 -0600

    Update simul.m
    Major re-haul:
    - simul.m is now a script! This doesn't really change much; to use it simply call "simul" in the command window as normal, but without the inputs. Once running, simul will automatically prompt you for the required inputs. Array inputs are separated with spaces rather than commas now. It will also remember your last set of inputs for the session, so you only need to enter them once, or modify the ones already there. You can also run simul by simply pressing shift+enter while in the file.
    - New "Parts" parameter! Before, simul would only accept an array of probabilities, assuming that you only wanted at least one of each part associated with a probability. But sometimes you want at least two of one part and three of another. Well now you can specify that by entering an array of the number of each part you want with respect to the probabilities you entered beforehand. Leaving this entry blank will just default to at least one of each part.
    - Simulation now uses an array of random ints rather than just one random int at a time, significantly reducing simulation time!
        - E.g. Simulating coin flips 1 million times that may have taken ~50 seconds before will now only take ~11 seconds.
    - The progress bar now shows what iteration simul is on and how many samples have succeeded so far! It also updates at more regular intervals, at most once a second, and can be set to update more or less frequently. However, it no longer estimates the time remaining until completion. Instead it will display how much time has elapsed.
        - The ETA was taken out because simul will actually accelerate as time goes on, making an ETA too unreliable.
    - simul is now more memory friendly, clearing variable that will no longer be used through the rest of the script as it goes!
    - Plots are now bar graphs to make it easier to line up data points with their x values.
        - Color does still change along the x-axis
        - Interpolated points are still plotted
        - Plots are no longer on a log scale in the x-axis
    - The command window now outputs the median and mode of the data as well. Before, it only showed the time elapsed, expected, and nearly guaranteed values.
    - Made fixes to the frequency(), trapezoid(), and reverse_trapezoid() functions popping errors every now and again, especially when the sample size was low.
        - Also got rid of the IQI() function reverse_trapezoid() was using and replaced it with MatLab's builtin fzero() function.
    - Added a new function, averageRun()! When only one of each part is needed, this function can find the exact average based on the probabilities themselves rather than calculating the average of the simulated data.

--- simul.m
+++ simul.m
@@ -1,7 +1,6 @@
-function simul(varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% --- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % FINNER
-% July 13th, 2020
+% September 15th, 2020
 % Drop Simulation
 
 % For a specified number of iterations and desired probibilities simul()
@@ -11,116 +10,217 @@ function simul(varargin)
 % reached.
 
 % Inputs:
-% Any list of real, positive probabilities or integers
+% Sample Size --- Number of players to simulate.
+% Probabilities - The probability of each desired part.
+%                 (separated by spaces, not commas)
+% Parts --------- The number of parts desired per probability.
+%                 (separated by spaces, not commas)
 
 % Outputs:
 % None
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% -Error Checks- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for i=1:nargin % Check for any negative or zero valued chances
-    if(varargin{i} <= 0)
-        error('Chances must be greater than zero.');
-    end
+defs = {}; n = 0;
+if exist('samples', 'var')
+    defs{1} = num2str(samples);
+else
+    defs{1} = '500000';
+end
+if exist('probabilities', 'var')
+    defs{2} = num2str(probabilities);
+    n = length(probabilities);
+else
+    defs{2} = '';
+end
+if exist('parts', 'var')
+    defs{3} = num2str(parts);
+elseif n > 0
+    defs{3} = num2str(ones([1 n]));
+else
+    defs{3} = '';
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%% -Begin Simulation & Plot- %%%%%%%%%%%%%%%%%%%%%%%%
-samples = 1000000; % number of iterations to simulate
+inputs = inputdlg({'Number of Samples',...
+    'Probabilities', 'Parts'},...
+    'Sample Size', [1 50; 1 50; 1 50], defs);
 
-n = nargin;
-probs = cell2mat(varargin);
+if isempty(inputs)
+    clearvars -except samples probabilities parts
+    return
+end
+
+if isempty(inputs{1})
+    if ~exist('samples', 'var')
+        samples = 500000;
+    end
+else
+    samples = str2num(inputs{1});
+end
+if floor(samples) ~= ceil(samples) || samples <= 0
+    clearvars -except samples probabilities parts
+    error('Sample size must be a positive whole number.');
+end
+assignin('base', 'samples', samples);
+
+if isempty(inputs{2})
+    if ~exist('probabilities', 'var')
+        clearvars -except samples probabilities parts
+        error('No probability array was given nor already exists.');
+    end
+else
+    probabilities = str2num(inputs{2});
+end
+n = length(probabilities);
+for i=1:n
+    if(probabilities(i) <= 0)
+        clearvars -except samples probabilities parts
+        error('Probabilities must be greater than zero.');
+    end
+end
+assignin('base', 'probabilities', probabilities); probs = probabilities;
+
+if isempty(inputs{3})
+    if ~exist('parts', 'var')
+        parts = ones([1 n]);
+    end
+elseif length(str2num(inputs{3})) ~= n
+    clearvars -except samples probabilities parts
+    error([num2str(n) ' probabilties were given, but '...
+        num2str(length(str2num(inputs{3}))) ' part counts were specified.']);
+else
+    parts = str2num(inputs{3});
+end
+for i=1:length(parts)
+    if floor(parts(i)) ~= ceil(parts(i)) || parts(i) <= 0
+        clearvars -except samples probabilities parts
+        error('Part counts must be positive whole numbers.');
+    end
+end
+assignin('base', 'parts', parts);
+clear defs inputs
+
+%%%%%%%%%%%%%%%%%%%%%%%% -Begin Simulation & Plot- %%%%%%%%%%%%%%%%%%%%%%%%
 if sum(probs) >= 1 % if total probability exeeds 100%, use their weights
     probs = probs / sum(probs);
 end
-x_vals = 0:n; sample_data = 0; null_prob = 1-sum(probs);
+null_prob = 1-sum(probs);
 
 if  null_prob < 0
     null_prob = 0;
 end
 
-tic; t = 0; time = 0; del = 1/samples;
-str = sprintf(['Progress: 0%%\n',...
-    'ETA: 0 Seconds, 0 Minutes, 0 Hours, 0 Days']);
+currCount = zeros([samples n+1]); y_vals = [];
+num = 0; bool = false;
+
+tic; t = 0;
+str = sprintf(['Iteration: 1',...
+    '\nSuccesses: ' addComma(num) '/' addComma(samples), '\nProgress: 0%%'...
+    '\nTime Elapsed: 00 Seconds, 00 Minutes, 00 Hours, 0 Days']);
 f = waitbar(0, str, 'Name', 'Simulating...', 'CreateCancelBtn',...
     'setappdata(gcbf, ''canceling'', 1)');
-setappdata(f, 'canceling', 0);
-for k = 1:samples
+setappdata(f, 'canceling', 0); k = 1;
+while (num < samples) && (k < 1000000)
     if getappdata(f, 'canceling')
         delete(f);
+        clearvars -except samples probabilities parts
         return
     end
-    count = zeros(n + 1, 1);
-    while sum(count) < samples
-        if getappdata(f, 'canceling')
-            delete(f);
-            return
-        end
-        j = randsample(x_vals, 1, true, [null_prob probs]);
-        if j >= 1
-            count(j) = count(j) + 1;
-        else
-            count(n + 1) = count(n + 1) + 1;
-        end
-        
-        if sum(count(1:n) >= 1) == n
-            break;
-        end
-    end
-    if length(sample_data) < sum(count)
-        sample_data((length(sample_data) + 1):sum(count)) =...
-            zeros(sum(count)-length(sample_data), 1);
-    end
-    sample_data(sum(count)) = sample_data(sum(count)) + 1;
     
-    if mod(k/samples, del) < 1/samples
+    vals = randsample(0:n, samples-num, true, [null_prob probs])';
+    
+    for i=1:n+1
         if getappdata(f, 'canceling')
             delete(f);
+            clearvars -except samples probabilities parts
             return
         end
         
-        i = 100*k/samples;
-        t_old = t; t = toc; time = time + (t - t_old);
-        eta = time/k; eta = eta*(samples - k);
-        eta = eta/60/60/24; days = fix(eta); hrs = fix((eta - days)*24);
-        mins = fix(((eta - days)*24 - hrs)*60);
-        secs = fix((((eta - days)*24 - hrs)*60 - mins)*60);
-        eta = [num2str(secs) ' Seconds, ' num2str(mins) ' Minutes, '...
-            num2str(hrs) ' Hours, ' num2str(days) ' Days'];
-        str = sprintf(['Progress: ', num2str(floor(i)), '%%\n',...
-            'ETA: ', eta]);
-        waitbar(i/100, f, str);
-        
-        if (t - t_old) < 0.5
-            del = del*2;
-        elseif (t - t_old) > 1.5
-            del = del/2;
-        end
+        currCount(:,i) = currCount(:,i) + (vals == i-1);
+        t = updateProgress(num, samples, t, f, k);
     end
+    
+    rows = []; prevArr = [];
+    for i=1:n
+        if getappdata(f, 'canceling')
+            delete(f);
+            clearvars -except samples probabilities parts
+            return
+        end
+        
+        [arr, ~] = find(vals == i);
+        rows(length(prevArr)+1:length(prevArr)+length(arr),1) = arr;
+        prevArr = arr;
+        t = updateProgress(num, samples, t, f, k);
+    end
+    
+    deleteRows = zeros([1 length(rows)]);
+    for i=1:length(rows)
+        if getappdata(f, 'canceling')
+            delete(f);
+            clearvars -except samples probabilities parts
+            return
+        end
+        
+        for j=2:n+1
+            if currCount(rows(i),j) >= parts(j-1)
+                bool = true;
+            else
+                bool = false;
+                break;
+            end
+            
+            t = updateProgress(num, samples, t, f, k);
+        end
+        
+        if bool
+            index = sum(currCount(rows(i),:));
+            if index > length(y_vals)
+                y_vals(index,1) = 0;
+            end
+            y_vals(index,1) = y_vals(index,1) + 1;
+            deleteRows(i) = rows(i);
+            
+            num = num + 1;
+            bool = false;
+        end
+        
+        t = updateProgress(num, samples, t, f, k);
+    end
+    
+    deleteRows(deleteRows == 0) = [];
+    currCount(deleteRows,:) = [];
+    k = k + 1;
 end
-waitbar(1, f, 'Simulation Finished!'); pause(1); delete(f);
+waitbar(1, f, 'Simulation Finished!'); pause(1); delete(f); k = k - 1;
+clearvars -except samples probabilities parts y_vals k n probs
 
-max_n = length(sample_data);
-x_vals = 1:max_n; y_vals = sample_data;
-d_x_vals = x_vals(y_vals > 0); d_y_vals = y_vals(y_vals > 0);
-average = sum(d_x_vals.*d_y_vals)/samples;
+x_vals = (sum(parts):k)';
+if sum(parts) > 1
+    y_vals(1:sum(parts)-1) = [];
+end
 
-x_vals = linspace(d_x_vals(1), d_x_vals(end), 10000);
-y_vals = x_vals;
-
-[~, i] = max(x_vals >= average);
-y_vals1 = interp1(d_x_vals, d_y_vals, x_vals, 'spline');
-y_vals2 = interp1(d_x_vals, d_y_vals, x_vals, 'pchip');
-y_vals(1:i) = y_vals1(1:i); y_vals(i+1:end) = y_vals2(i+1:end);
+if n == sum(parts)
+    average = averageRun(probs);
+else
+    average = sum(x_vals.*y_vals)/samples;
+end
 
 mode = x_vals(y_vals == max(y_vals));
 [average_p, mode_p] = trapezoid_data(average, mode, x_vals, y_vals);
 
-[median, ng] = reverse_trapezoid(d_x_vals, d_y_vals);
+[median, ng] = reverse_trapezoid(x_vals, y_vals);
 range = round((ng(3) - ng(1))/2, 0);
 
-varience = sum(d_y_vals.*(d_x_vals.^2))/samples - average^2;
+varience = sum(y_vals.*(x_vals.^2))/samples - average^2;
 std_err = sqrt(varience);
-se_int = [-std_err; std_err];
-average_int(1:2) = 1.96*se_int + average;
+
+d_x_vals = x_vals(y_vals > 0); d_y_vals = y_vals(y_vals > 0);
+x_vals = linspace(d_x_vals(1), d_x_vals(end), 10000); y_vals = x_vals;
+[~, i] = max(x_vals >= ng(1));
+y_vals1 = interp1(d_x_vals, d_y_vals, x_vals, 'spline');
+y_vals2 = interp1(d_x_vals, d_y_vals, x_vals, 'pchip');
+y_vals(1:i) = y_vals1(1:i); y_vals(i+1:end) = y_vals2(i+1:end);
+y_vals = abs(y_vals);
 
 time = toc/60/60/24; days = fix(time); hrs = fix((time - days)*24);
 mins = fix(((time - days)*24 - hrs)*60);
@@ -129,20 +229,13 @@ secs = (((time - days)*24 - hrs)*60 - mins)*60;
 %
 figure; % from here on is mostly just plotting customization
 cmap = colormap(jet(length(x_vals)));
-set(gcf, 'position', [10 50 800 600]);
+set(gcf, 'position', [10 50 800 600]); d_y_vals = d_y_vals/samples;
+b = bar(d_x_vals, d_y_vals, 'FaceColor', 'flat'); hold on
 c = linspace(0, 1, length(x_vals)); y_vals = y_vals/samples;
-scatter(x_vals, y_vals, 0.5, c); hold on
-c = interp1(x_vals, c, d_x_vals); d_y_vals = d_y_vals/samples;
-scatter(d_x_vals, d_y_vals, 36, c);
+scatter(x_vals, y_vals, 0.5, c); c = interp1(x_vals, c, d_x_vals);
+b.CData = c';
 yt = get(gca, 'YTick'); delta = (yt(end) - yt(1))/(length(yt) - 1);
-
 yLim = [0 yt(end)+2*delta]; set(gca, 'YLim', yLim);
-str = ['  CI_{50%%}= ' char(177), num2str(0.6745*std_err,'%.5f')...
-    '    CI_{75%%}= ' char(177), num2str(1.15*std_err,'%.5f')...
-    '    CI_{90%%}= ' char(177), num2str(1.645*std_err,'%.5f')...
-    '    CI_{95%%}= ' char(177), num2str(1.96*std_err,'%.5f')...
-    '    CI_{99%%}= ' char(177), num2str(2.576*std_err,'%.5f')];
-text(n, yLim(2)*0.0275, sprintf(str));
 
 val = mode; [~, i] = max(x_vals >= val);
 plot([val val], yLim, 'Color', cmap(i, :));
@@ -188,21 +281,52 @@ text(val, yLim(2)*0.625,...
 
 title(sprintf(['Number of Runs for ' addComma(samples) ' Samples']));
 xlabel('Number of Runs'); ylabel('Frequency');
-xlim([0 max_n]); ax = gca; ax.YGrid = 'on'; ax.XGrid = 'on';
-set(gca, 'xscale', 'log');
+xlim([sum(parts)-1 k]); ax = gca; ax.YGrid = 'on'; ax.XGrid = 'on';
+%set(gca, 'xscale', 'log');
+
+str = ['  CI_{50%%}= ' char(177), num2str(0.6745*std_err,'%.5f')...
+    '    CI_{75%%}= ' char(177), num2str(1.15*std_err,'%.5f')...
+    '    CI_{90%%}= ' char(177), num2str(1.645*std_err,'%.5f')...
+    '    CI_{95%%}= ' char(177), num2str(1.96*std_err,'%.5f')...
+    '    CI_{99%%}= ' char(177), num2str(2.576*std_err,'%.5f')];
+text(x_vals(1), yLim(2)*0.0275, sprintf(str));
 hold off
 %}
 
 fprintf(['   Time Elapsed      = ' num2str(secs) ' Seconds, '...
     num2str(mins) ' Minutes, ' num2str(hrs) ' Hours, ' num2str(days)...
     ' Days\n']);
-fprintf(['   Expected          = ' num2str(average_int(1)) ' - '...
-    num2str(average_int(2)) ' (' num2str(average) ' - '...
-    num2str(average_p*100) '%%)\n']);
+fprintf(['   Expected          = ' num2str(average) ' - '...
+    num2str(average_p*100) '%%\n']);
+fprintf(['   Median            = ' num2str(median) ' - 50%%\n']);
+fprintf(['   Mode              = ' num2str(mode) ' - '...
+    num2str(mode_p*100) '%%\n']);
 fprintf(['   Nearly Guaranteed = ' num2str(round(ng(2))) ' '...
     char(177) ' ' num2str(range) '\n\n']);
-end
+clearvars -except samples probabilities parts
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% --- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+function [t] = updateProgress(num, samples, t, f, k, del)
+if(nargin < 7)
+    del = 1;
+end
+t_curr = toc;
+if (t_curr - t) >= del
+    m = 100*num/samples;
+    time = t_curr/60/60/24; days = fix(time); hrs = fix((time - days)*24);
+    mins = fix(((time - days)*24 - hrs)*60);
+    secs = fix((((time - days)*24 - hrs)*60 - mins)*60);
+    te = sprintf('%02d Seconds,', secs);
+    te = strcat(te, sprintf(' %02d Minutes,', mins));
+    te = strcat(te, sprintf(' %02d Hours,', hrs));
+    te = strcat(te, sprintf(' %0d Days', days));
+    str = sprintf(['Iteration: ' addComma(k),...
+        '\nSuccesses: ' addComma(num) '/' addComma(samples),...
+        '\nProgress: ' num2str(floor(m)) '%%', '\nTime Elapsed: ' te]);
+    waitbar(m/100, f, str);
+    t = toc;
+end
+end
 
 function [frequency] = freq(x, x_vals, y_vals)
 n = length(x_vals);
@@ -210,7 +334,13 @@ int = x_vals(end) - x_vals(1);
 delta = int/(n - 1);
 
 x1 = floor((x - x_vals(1))/delta + x_vals(1));
+if x1 <= 0
+    x1 = 1;
+end
 x2 = ceil((x - x_vals(1))/delta + x_vals(1));
+if x2 <= 0
+    x2 = 1;
+end
 
 if x_vals(x2) - x_vals(x1) <= 0
     frequency = y_vals(x1);
@@ -238,19 +368,24 @@ int = x_vals(end) - x_vals(1);
 delta = int/(n - 1);
 total = trapezoid_data_step(x_vals, y_vals);
 
-avg1 = floor((average - x_vals(1))/delta + x_vals(1));
-avg2 = ceil((average - x_vals(1))/delta + x_vals(1));
-mode1 = floor((mode - x_vals(1))/delta + x_vals(1));
+avg_ind1 = floor((average - x_vals(1))/delta + x_vals(1)) - x_vals(1) + 1;
+avg_ind2 = ceil((average - x_vals(1))/delta + x_vals(1)) - x_vals(1) + 1;
+mode_ind = floor((mode - x_vals(1))/delta + x_vals(1)) - x_vals(1) + 1;
 
-y_avg = (y_vals(avg2) - y_vals(avg1))*(average - x_vals(avg1))...
-    /(x_vals(avg2) - x_vals(avg1)) + y_vals(avg1);
-integral = (y_avg + y_vals(avg1))*(average - x_vals(avg1))/2;
-integral = integral + trapezoid_data_step(x_vals(1:avg1), y_vals(1:avg1));
+if avg_ind1 ~= avg_ind2
+    y_avg = (y_vals(avg_ind1) - y_vals(avg_ind2))*(x_vals(avg_ind1) -...
+        average)/(x_vals(avg_ind1) - x_vals(avg_ind2)) + y_vals(avg_ind2);
+else
+    y_avg = y_vals(avg_ind1);
+end
+
+integral = (y_avg + y_vals(avg_ind1))*(average - x_vals(avg_ind1))/2;
+integral = integral + trapezoid_data_step(x_vals(1:avg_ind1),...
+    y_vals(1:avg_ind1));
 avg_p = integral/total;
 
-integral = trapezoid_data_step(x_vals(1:mode1), y_vals(1:mode1));
+integral = trapezoid_data_step(x_vals(1:mode_ind), y_vals(1:mode_ind));
 mode_p = integral/total;
-
 end
 
 function [point, ng] = reverse_trapezoid(x_vals, y_vals)
@@ -272,18 +407,20 @@ ng = [0.5 0.99 0.999 0.9999];
 
 i = n;
 for j = 4:-1:1
-    while integral(i)/total < 1 - ng(j)
+    while (integral(i)/total) < (1 - ng(j))
         i = i - 1;
     end
-    area = 1 - ng(j) - integral(i+1)/total;
-    y = @(x) (y_vals(i+1) - y_vals(i))*(x_vals(i+1) - x)...
-        /(x_vals(i+1) - x_vals(i)) + y_vals(i+1);
-    func = @(x) (y_vals(i+1) + y(x))*(x_vals(i+1) - x)/total - area;
-    ng(j) = IQI(func, x_vals(i), (x_vals(i)+x_vals(i+1))/2, x_vals(i+1));
+    
+    area = 1 - integral(i+1)/total - ng(j);
+    y = @(x) (y_vals(i) - y_vals(i+1))*(x - x_vals(i+1))...
+        /(x_vals(i) - x_vals(i+1)) + y_vals(i+1);
+    func = @(x) area - (y_vals(i+1) + y(x))*(x_vals(i+1) - x)/total/2;
+    ng(j) = fzero(func, [x_vals(i) x_vals(i+1)]);
+    i = i + 1;
 end
+
 point = ng(1);
 ng = ng(2:4);
-
 end
 
 function [integral] = trapezoid_data_step(x_vals, y_vals)
@@ -302,99 +439,32 @@ else
             y_vals(i+1))/2;
     end
 end
-
 end
 
 
-function [x_root,func_val,error_approx,num_iterations] = IQI(func,...
-    x_guess_one,x_guess_two,x_guess_three,error_desired,max_iterations)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -Error Checks- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if(nargin < 4) % Check if less than four inputs
-    error('At least four arguments are required.');
-end
-if(x_guess_one == x_guess_two) % Check if both guesses are the same
-    error('x_guess_one must not be equal to x_guess_two.');
-end
-if(x_guess_one == x_guess_three) % Check if both guesses are the same
-    error('x_guess_one must not be equal to x_guess_three.');
-end
-if(x_guess_two == x_guess_three) % Check if both guesses are the same
-    error('x_guess_two must not be equal to x_guess_three.');
-end
-if(nargin < 5) || isempty(error_desired) % Default error_desired if not
-    error_desired = 0.0001;              % given
-end
-if(nargin < 6) || isempty(max_iterations) % Default max_iterations if not
-    max_iterations = 50;                  % given
-end
-if(max_iterations <= 0) % Check if max_iterations is greater than zero
-    error('max_iterations must be greater than zero.');
+function [runs] = averageRun(varargin)
+probs = cell2mat(varargin); sumP = sum(probs);
+if sumP <= 0
+    runs = 0;
+    return
+elseif sumP > 1
+    probs = probs/sumP;
+    sumP = 1;
 end
 
-%%%%%%%%%%%%%%%%% -Begin Inverse Quadratic Interpolation- %%%%%%%%%%%%%%%%%
-% Initialize values
-x_old_old = x_guess_one; x_old = x_guess_two; x_current = x_guess_three;
-error_approx = error_desired + 1;
-num_iterations = 0;
-
-if(func(x_old_old) == 0)
-    x_root = x_old_old;
-    func_val = func(x_old_old);
-    error_approx = 0;
-    num_iterations = 0;
-elseif(func(x_old) == 0)
-    x_root = x_old;
-    func_val = func(x_old);
-    error_approx = 0;
-    num_iterations = 0;
-elseif(func(x_current) == 0)
-    x_root = x_current;
-    func_val = func(x_current);
-    error_approx = 0;
-    num_iterations = 0;
-else
-    while(error_approx > error_desired)&& (num_iterations < max_iterations)
-        num_iterations = num_iterations + 1;
-        if(func(x_current) ~= func(x_old)&& func(x_old) ~=...
-                func(x_old_old)&& func(x_current) ~= func(x_old_old))
-            x_new = (func(x_old)*func(x_old_old)/((func(x_current)-...
-                func(x_old))*(func(x_current)-func(x_old_old))))*...
-                x_current+(func(x_current)*func(x_old_old)/((func(x_old)...
-                -func(x_current))*(func(x_old)-func(x_old_old))))*x_old+...
-                (func(x_current)*func(x_old)/((func(x_old_old)-...
-                func(x_current))*(func(x_old_old) - func(x_old))))*...
-                x_old_old;
-        else
-            if(func(x_current) == func(x_old))
-                error(['x_current and x_old have equal function values,'...
-                    ' resulting in a division by zero. Try a different'...
-                    ' guess two or guess three.'])
-            elseif(func(x_current) == func(x_old_old))
-                error(['x_current and x_old_old have equal function'...
-                    ' values, resulting in a division by zero. Try a'...
-                    ' different guess one or guess three.'])
-            else
-                error(['x_old and x_old_old have equal function'...
-                    ' values, resulting in a division by zero. Try a'...
-                    ' different guess one or guess two.'])
-            end
-        end
-        if(x_new ~= 0)
-            error_approx = abs((x_new - x_current)/x_new) * 100;
-        end
-        x_old_old = x_old;
-        x_old = x_current;
-        x_current = x_new;
-    end
-    x_root = x_new;
-    func_val = func(x_root);
+runs = 1;
+for i = 1:length(probs)
+    probArray = probs; probArray(i) = [];
+    run = averageRun(probArray);
+    runs = runs + probs(i)*run;
 end
 
+runs = runs/sumP;
 end
+
 
 function numOut = addComma(numIn)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% -Begin Add Comma- %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 jf=java.text.DecimalFormat; % comma for thousands, three decimal places
 numOut= char(jf.format(numIn)); % omit "char" if you want a string out
-
 end
